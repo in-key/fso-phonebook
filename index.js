a5e2d0673090ec62994c38f7ce88313572ccc89d
in-key <inkeydev@gmail.com>
2023-01-15 18:25:11 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const Person = require('./models/person');
 
 app.use(cors());
 app.use(express.json());
@@ -43,51 +45,105 @@ let phonebook = [
     }
 ];
 
-app.get('/api/persons', (req, res) => {
-    res.json(phonebook);
+app.get('/api/persons', (req, res, next) => {
+    Person.find({}).then( people => {
+        res.json(people);
+    })
+    .catch(e => next(e));
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<div>Phonebook has info for ${phonebook.length} people</div><div>${new Date()}}</div>`)
+app.get('/info', (req, res, next) => {
+    Person.find({}).then( people => {
+        res.send(`<div>Phonebook has info for ${people.length} people</div><div>${new Date()}}</div>`)
+    })
+    .catch(e => next(e));
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = phonebook.find( person => person.id === id );
-    if (person){
-        res.json(person);
-    } else {
-        res.status(404);
-        res.json({message: "person not found"});
-    }
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    phonebook = phonebook.filter( person => person.id !== id);
-    res.status(204).end();
-})
-
-app.post('/api/persons', (req, res) => {
-    const person = req.body;
-    if (!person.name || !person.number){
-        res.status(400);
-        res.json({ error: 'name and number are required' });
-        return;
-    }
-
-    for (let p of phonebook){
-        if (p.name === person.name){
-            res.status(400);
-            res.json({ error: 'name must be unique' });
-            return;
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id).then( person => {
+        if (person){
+            res.json(person);
+        } else {
+            res.status(404);
+            res.json({message: "person not found"});
         }
+    })
+    .catch( e => next(e))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+    const id = req.params.id;
+    // phonebook = phonebook.filter( person => person.id !== id);
+    Person.findByIdAndRemove(id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
+    const {name, number} = req.body;
+    // if (!name || !number){
+    //     res.status(400);
+    //     res.json({ error: 'name and number are required' });
+    //     return;
+    // }
+
+    // for (let p of phonebook){
+    //     if (p.name === name){
+    //         res.status(400);
+    //         res.json({ error: 'name must be unique' });
+    //         return;
+    //     }
+    // }
+
+    Person.findOne({name: name}).then( returnedPerson => {
+        if (returnedPerson){
+            res.status(400).json({
+                error: `${name} already exists in phonebook`
+            })
+        } else {
+            const person = new Person({
+                name: name,
+                number: number
+            });
+
+            person.save().then( savedPerson => {
+                res.json( savedPerson );
+            })
+            .catch( e => next(e));
+        }
+    })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const {name, number} = req.body;
+
+    const person = {
+        name: name,
+        number: number
+    };
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true, runValidators: true, context: 'query'})
+        .then( updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return res.status(400).json({ error: error.message })
     }
 
-    person.id = Math.floor(Math.random() * 10000000);
-    phonebook = phonebook.concat(person);
-    res.json(person);
-})
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
